74e6a0571fb4501be169c3264020a5c89492b27c
Larissa Albuquerque <105246799+larialbu@users.noreply.github.com>
2022-10-03 17:38:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function clique(){
+    let casas = prompt("DIGITE SEU PERSONAGEM: ");
+
+    switch (casas) {
+        case 'Harry Potter':
+        case 'Alvo Dumbledore':
+        case 'Minerva McGonagall':
+          alert('Grifinoria!');
+          break;
+        case 'Luna Lovegood':
+        case 'Gilderoy Lockhart':
+        case 'Garrick Olivaras':
+          alert('Corvinal!');
+          break;
+        case ' Newt Scamander':
+        case 'Cedrico Diggory':
+        case 'Ninfadora Tonks':
+          alert('Lufa-Lufa!');
+          break;
+        case 'Tom Riddle':
+        case 'Draco Malfoy':
+        case 'Dolores Umbridge':
+          alert('Sonserina!');
+      }
+}
+
+
+
